382c09f5b6191a33ee35387f2517bf1a2303d66d
Bushra Patel <bpatel60@yahoo.com>
2024-03-25 13:38:53 -0400

    Assertion values for test Corrected when Comparing Arrays Equality

--- eqArrays.js
+++ eqArrays.js
@@ -13,6 +13,7 @@ const eqArrays = function(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         result = false;
+        return result;
       }
     }
   } else {
