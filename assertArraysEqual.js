bf944e2c874ba661a153f3bdbcaaee4e57fe0538
Bushra Patel <bpatel60@yahoo.com>
2024-03-25 14:16:21 -0400

    Asserting two arrays with the hel of eqArrays function and printing the result

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,5 @@
 const eqArrays = function(arr1, arr2) {
-  let result = true;
-
+  
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
