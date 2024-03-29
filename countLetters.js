a0651fa04806bd5fd8f561dcd102d316dd775ed3
Bushra Patel <bpatel60@yahoo.com>
2024-03-29 11:50:58 -0400

    Counting letters of a string and returning result as an Object

--- countLetters.js
+++ countLetters.js
@@ -25,7 +25,6 @@ const countLetters = function(sentence) {
 const result1 = countLetters("lighthouse in the house");
 
 assertEqual(result1["l"], 1);
-assertEqual(result1[" "], undefined);
 assertEqual(result1["i"], 2);
 assertEqual(result1["h"], 4);
 assertEqual(result1["e"], 3);
