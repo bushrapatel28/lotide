72a75372852e9787a933b5bcddfb434255befd2d
Bushra Patel <bpatel60@yahoo.com>
2024-03-30 05:44:51 -0400

    Asserting Equality of key-value pair of Objects. Primitive values ONLY

--- eqObjects.js
+++ eqObjects.js
@@ -14,17 +14,24 @@ const eqObjects = function(object1, object2) {
   if (arrOfObjKeys1.length !== arrOfObjKeys2.length) {
     return false;
   } else {
-    for (let i = 0; i < arrOfObjKeys1.length; i++) {
-      for (let j = 0; j < arrOfObjKeys2.length; j++) {
-        if (arrOfObjKeys1[i] === arrOfObjKeys2[j]) {
-          if (object1[arrOfObjKeys1[i]] === object2[arrOfObjKeys2[j]]) {
-            result = true;
-          } else {
-            return false;
-          }
-        }
+    for (const [key, value] of Object.entries(object1)) {
+      if (object1[key, value] === object2[key, value]) {
+        result = true;
+      } else {
+        return false;
       }
     }
+    // for (let i = 0; i < arrOfObjKeys1.length; i++) {
+    //   for (let j = 0; j < arrOfObjKeys2.length; j++) {
+    //     if (arrOfObjKeys1[i] === arrOfObjKeys2[j]) {
+    //       if (object1[arrOfObjKeys1[i]] === object2[arrOfObjKeys2[j]]) {
+    //         result = true;
+    //       } else {
+    //         return false;
+    //       }
+    //     }
+    //   }
+    // }
   }
   return result;
 };
