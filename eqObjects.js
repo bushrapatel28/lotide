0b7a3335b556b6c8069830f20cb5d10cd4fd5ed4
Bushra Patel <bpatel60@yahoo.com>
2024-03-30 22:17:40 -0400

    Checking Objects Equality with Primitive Values ONLY

--- eqObjects.js
+++ eqObjects.js
@@ -14,29 +14,19 @@ const eqObjects = function(object1, object2) {
   if (arrOfObjKeys1.length !== arrOfObjKeys2.length) {
     return false;
   } else {
-    for (let key1 of arrOfObjKeys1) {   //Looping over keys of object1
-      for(let key2 of arrOfObjKeys2) {  //Looping over keys of object2
-        if(key1 === key2) {             //Checking for keys equality
-          if(object1[key1] === object2[key2]) {    //Equality Check for Object key's values
-            result = true;
-          } else {
-            return false;
-          }
-        } 
+    for (let key of arrOfObjKeys1) {            //Looping over keys of object1
+      if (arrOfObjKeys2.includes(key)) {         //Checking if the same key exists in object2
+        if (object1[key] !== object2[key]) {
+          return false;
+        } else {
+          result = true;
+        }
+      } else {
+        result = false;
+        break;
       }
     }
   }
-    // for (let i = 0; i < arrOfObjKeys1.length; i++) {
-    //   for (let j = 0; j < arrOfObjKeys2.length; j++) {
-    //     if (arrOfObjKeys1[i] === arrOfObjKeys2[j]) {
-    //       if (object1[arrOfObjKeys1[i]] === object2[arrOfObjKeys2[j]]) {
-    //         result = true;
-    //       } else {
-    //         return false;
-    //       }
-    //     }
-    //   }
-    // }
   return result;
 };
 
