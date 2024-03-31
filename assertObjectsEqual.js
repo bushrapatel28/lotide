7bad05ed9b8497fea2f97ee807a771f85c97a635
Bushra Patel <bpatel60@yahoo.com>
2024-03-30 21:37:17 -0400

    Checking Objects Equality

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,17 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
   const arrOfObjKeys1 = Object.keys(object1);
   const arrOfObjKeys2 = Object.keys(object2);
@@ -6,11 +20,17 @@ const eqObjects = function(object1, object2) {
   if (arrOfObjKeys1.length !== arrOfObjKeys2.length) {
     return false;
   } else {
-    for (const [key, value] of Object.entries(object1)) {
-      if (object1[key] === object2[key]) {
-        result = true;
-      } else {
-        return false;
+    for (let key1 of arrOfObjKeys1) {   //Looping over keys of object1
+      for(let key2 of arrOfObjKeys2) {  //Looping over keys of object2
+        if(key1 === key2) {             //Checking for keys equality
+          if(Array.isArray(object1[key1]) || Array.isArray(object2[key2])) { //Equality check for Object key's array values
+          result = eqArrays(object1[key1], object2[key2]);
+          } else if(object1[key1] === object2[key2]) {    //Equality Check for Object key's values
+            result = true;
+          } else {
+            return false;
+          }
+        } 
       }
     }
   }
