3be8959a53aea7477ad61ba18c5607ddcb492bc4
Bushra Patel <bpatel60@yahoo.com>
2024-04-01 16:38:09 -0400

    Finding the Key of a value using CallBacks

---
+++ findKey.js
@@ -0,0 +1,54 @@
+//Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(objectToScan, callBack) {
+  const objectKeys = Object.keys(objectToScan);
+  
+  for (let key of objectKeys) {
+   
+    if (callBack(objectToScan[key])) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+), "noma");
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 3
+), "Akaleri");
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 0
+), undefined);
+
