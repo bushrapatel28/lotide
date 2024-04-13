f5467728e6b19ad2d3ac31c6620b9ea60c118f01
Bushra Patel <bpatel60@yahoo.com>
2024-04-13 12:47:38 -0400

    Refactored

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let returnedValue = eqArrays(arr1, arr2);
-
-  if (returnedValue) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(arr) {
   const size = arr.length;
   let mid = Math.floor(size / 2);
@@ -41,9 +17,4 @@ const middle = function(arr) {
   return result;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
