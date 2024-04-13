7eefe60b5f56df294981c7c135d515e5e5f9acd9
Bushra Patel <bpatel60@yahoo.com>
2024-04-12 21:00:26 -0400

    Refactored

--- tail.js
+++ tail.js
@@ -1,26 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+const assertEqual = require("./assertEqual");
 
 const tail = function(arr) {
   let newArr = arr.slice(1);
   return newArr;
 };
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// for (let t = 0; t < result.length; t++) {
-//   assertEqual(result[t], ["Lighthouse", "Labs"][t]);
-// }
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-tail([5]);
-assertEqual(tail([5]).length, 0);
-tail([]);
-assertEqual(tail([]).length, 0);
+module.exports = tail;
