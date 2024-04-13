1b7dbbce744d598371a776e470e5ac4468c19bef
Bushra Patel <bpatel60@yahoo.com>
2024-04-12 21:00:50 -0400

    Testing Code

---
+++ test/tailTest.js
@@ -0,0 +1,16 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// for (let t = 0; t < result.length; t++) {
+//   assertEqual(result[t], ["Lighthouse", "Labs"][t]);
+// }
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+tail([5]);
+assertEqual(tail([5]).length, 0);
+tail([]);
+assertEqual(tail([]).length, 0);
