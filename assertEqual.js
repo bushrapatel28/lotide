7069c092eedcf4817ecf033d4fd6d834e8ff707e
Bushra Patel <bpatel60@yahoo.com>
2024-04-12 20:05:38 -0400

    Refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,10 @@
 //Function Implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(6, 5);
-assertEqual("potato", 'potato');
+module.exports = assertEqual;
